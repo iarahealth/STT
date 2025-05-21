76e52d8b33976c4f1211653569579b6f03b6554f
Daniel Souza <dsouza1995@gmail.com>
2025-05-21 09:29:35 -0300

    debug: try importing the header directly

--- ci_scripts/host-build.sh
+++ ci_scripts/host-build.sh
@@ -16,7 +16,7 @@ source $(dirname "$0")/build-utils.sh
 source $(dirname "$0")/tf-vars.sh
 
 if [ "${OS}" = "Linux" ]; then
-  BAZEL_EXTRA_FLAGS+=" --copt=-I/opt/rh/gcc-toolset-12/root/usr/include/c++/12/"
+  BAZEL_EXTRA_FLAGS+=" --copt='-include /opt/rh/gcc-toolset-12/root/usr/include/c++/12/limits'"
 fi
 
 BAZEL_TARGETS="
