e1a8893591f3e0b4a375d5c9714e4c110158ea82
Daniel Souza <dsouza1995@gmail.com>
2025-05-27 14:46:41 -0300

    fix: disable macos cross-compilation for arm targets

--- ci_scripts/host-build.sh
+++ ci_scripts/host-build.sh
@@ -5,9 +5,6 @@ set -xe
 macos_target_arch=$1
 DISABLE_AVX=$2
 SYSTEM_TARGET=host
-if [ "$(uname)-$(uname -m)" = "Darwin-x86_64" -a "${macos_target_arch}" = "arm64" ]; then
-  SYSTEM_TARGET="darwin-arm64"
-fi
 
 source $(dirname "$0")/all-vars.sh
 source $(dirname "$0")/all-utils.sh
